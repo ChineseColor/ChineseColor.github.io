8e0c4de4696914011c7700f566002eedd4e7e502
AberSheeran <AberSheeran@qq.com>
2019-04-10 20:05:03 +0800

    优化首屏加载速度

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './pure.min.css';
 import App from './views/App';
 import * as serviceWorker from './serviceWorker';
 
