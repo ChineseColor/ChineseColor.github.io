9147a9fa8f3c4b80ac0665f9416e8b2d75fe9b44
abersheeran <me@abersheeran.com>
2020-03-12 23:47:31 +0800

    update

---
+++ precache-manifest.27584514f75f4b007df3c070f1c789e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9088fc1b47f3ce8e555e",
+    "url": "./static/css/main.a9fee350.chunk.css"
+  },
+  {
+    "revision": "9088fc1b47f3ce8e555e",
+    "url": "./static/js/main.4151ae8f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "dd955478d4aa7fcbf532",
+    "url": "./static/js/2.5b54e0d5.chunk.js"
+  },
+  {
+    "revision": "b3d8463326f2cad3df28cbcd1c63b9e5",
+    "url": "./index.html"
+  }
+];
