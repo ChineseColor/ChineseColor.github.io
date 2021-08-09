8443eecc0682b54422e8f016d3fec6cafb41ede2
abersheeran <me@abersheeran.com>
2021-08-09 11:48:19 +0000

    Deploying to gh-pages from @ ChineseColor/ChineseColor.github.io@15af9cfd24d54d72e792a6b66d11f4743fae7169 🚀

---
+++ precache-manifest.fc4489ca7814f304a1d0aba1e982657b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "678ba65a25d93fd37f54",
+    "url": "./static/css/main.04504f0f.chunk.css"
+  },
+  {
+    "revision": "678ba65a25d93fd37f54",
+    "url": "./static/js/main.a50353b0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "dd955478d4aa7fcbf532",
+    "url": "./static/js/2.5b54e0d5.chunk.js"
+  },
+  {
+    "revision": "ff14894ad6886300286f73e17c8e32c9",
+    "url": "./index.html"
+  }
+];
